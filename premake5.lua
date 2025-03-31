54d901ce0dfe79ed3607dd5a5f8b2f21175da6f5
Leander De Hertogh <leanderdehertogh@hotmail.com>
2025-03-31 13:12:26 +0200

    Fix gitignore and change lua file to make project compile

--- premake5.lua
+++ premake5.lua
@@ -8,14 +8,9 @@ project "GLFW"
 	files
 	{
 		"include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-		"src/vulkan.c",
-		"src/window.c"
+    	"include/GLFW/glfw3native.h",
+    	"src/**.h",
+    	"src/**.c",
 	}
 	
 	filter "system:windows"
